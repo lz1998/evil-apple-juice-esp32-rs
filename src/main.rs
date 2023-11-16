b94f03cc1517ccd9ce9fd5baa7dfcf46156f15d2
lz1998 <875543533@qq.com>
2023-11-16 22:15:42 +0800

    random addr and mode

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,22 @@
 
 mod devices;
 
+use esp32_nimble::enums::{ConnMode, OwnAddrType};
+use esp_idf_svc::sys::{esp_fill_random, random};
 use devices::DEVICES;
+
+fn random_addr() -> [u8; 6] {
+    let mut addr = [0u8, 0, 0, 0, 0, 0];
+    unsafe { esp_fill_random(addr.as_mut_ptr() as *mut core::ffi::c_void, 6) };
+    addr
+}
+
+fn random_mode() -> ConnMode {
+    match unsafe { random() } % 2 {
+        0 => ConnMode::Non,
+        _ => ConnMode::Und,
+    }
+}
 
 #[no_mangle]
 fn main() {
@@ -17,10 +32,13 @@ fn main() {
     log::info!("Hello, world!");
 
     let ble_device = esp32_nimble::BLEDevice::take();
+    ble_device.set_own_addr_type(OwnAddrType::Random);
     let ble_advertising = ble_device.get_advertising();
 
     for (name, data) in DEVICES.iter().cycle() {
         log::info!("{name}");
+        let _ = ble_device.set_rnd_addr(random_addr());
+        ble_advertising.advertisement_type(random_mode());
         ble_advertising.custom_adv_data(data).unwrap();
         ble_advertising.start().unwrap();
         esp_idf_hal::delay::FreeRtos::delay_ms(1000);
