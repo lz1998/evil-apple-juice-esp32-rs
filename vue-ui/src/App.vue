4ae2e2c2d7ba2d9ee15ee43a053729d72e3203cc
YLLA1215 <325936216@qq.com>
2023-12-16 22:16:34 +0800

    add a new window of message

--- vue-ui/src/App.vue
+++ vue-ui/src/App.vue
@@ -1,23 +1,63 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import { ref } from "vue";
 
-const handleStart = () => {
-  fetch("/api/start");
+// const duration = ref<number>(0);
+const message = ref<string>("");
+const messageVisible = ref<boolean>(false);
+
+const showMessage = (msg: string): void => {
+  message.value = msg;
+  messageVisible.value = true;
+  setTimeout(() => {
+    messageVisible.value = false;
+  }, 2000);
 };
 
-const handleStop = () => {
-  fetch("/api/stop");
+const handleStart = (): void => {
+  fetch("/api/start")
+    .then((res) => res.json())
+    .then((res) => {
+      showMessage("已开启");
+    });
+};
+
+const handleStop = (): void => {
+  fetch("/api/stop")
+    .then((res) => res.json())
+    .then((res) => {
+      showMessage("已关闭");
+    });
 };
 </script>
 
 <template>
-  <div class="buttonGroups">
-    <div class="buttonStart" @click="handleStart">Start</div>
-    <div class="buttonStop" @click="handleStop">Stop</div>
+  <div>
+    <Transition name="fade" appear>
+      <div class="message" v-if="messageVisible">{{ message }}</div>
+    </Transition>
+    <div class="buttonGroups">
+      <div class="buttonStart" @click="handleStart">开启</div>
+      <div class="buttonStop" @click="handleStop">关闭</div>
+    </div>
   </div>
 </template>
 
 <style>
+.message {
+  position: absolute;
+  top: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 10px 20px 10px 20px;
+  background-color: white;
+  border-radius: 3px;
+  box-shadow: rgba(0, 0, 0, 0.12) 0px 3px 6px -4px,
+    rgba(0, 0, 0, 0.08) 0px 6px 16px 0px, rgba(0, 0, 0, 0.05) 0px 9px 28px 8px;
+  color: rgb(51, 54, 57);
+  font-size: 14px;
+  z-index: 100;
+}
+
 .buttonGroups {
   position: absolute;
   top: 50%;
@@ -53,5 +93,15 @@ const handleStop = () => {
   justify-content: center;
   align-items: center;
   cursor: pointer;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
